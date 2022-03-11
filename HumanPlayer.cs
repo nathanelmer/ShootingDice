a9608b7d58335f0b761d0b527d13b440f0994063
Nathan Elmer <nelmer12@gmail.com>
2022-03-11 13:58:14 -0600

    phase 2 and 3

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,34 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
+        public override void Play(Player other)
+        {
+            // Call roll for "this" object and for the "other" object
+            Console.Write("Please enter a number 1-6: ");
+            int myRoll = int.Parse(Console.ReadLine());
+            int otherRoll = other.Roll();
 
+            Console.WriteLine($"{Name} rolls a {myRoll}");
+            Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+            if (myRoll > otherRoll)
+            {
+                Console.WriteLine($"{Name} Wins!");
+            }
+            else if (myRoll < otherRoll)
+            {
+                Console.WriteLine($"{other.Name} Wins!");
+            }
+            else
+            {
+                // if the rolls are equal it's a tie
+                Console.WriteLine("It's a tie");
+            }
+        }
     }
 }
